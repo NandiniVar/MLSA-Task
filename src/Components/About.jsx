75ac47a65d848d4ab952c9b0a98361da61507df6
Nandini Varshney <92244676+NandiniVar@users.noreply.github.com>
2023-11-02 21:35:44 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/building.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a web designer with a passion for technology and innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web designing",
-  "Ugithub",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Open Source Contributing",
+  "Cloud computing",
+  "Problem Solving",
+  "Data Structures and Algorithms",
+  ,
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I'm a creative web designer who combines my love for technology with a keen eye for design to craft compelling online experiences. My enthusiasm for staying at the forefront of digital trends drives me to create engaging and user-friendly websites..";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/candles.jpg";
+import image from "../images/study.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Linkedin Profile",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is my Linkedin Profile where you can find my work experience and skills.",
+    url: "https://www.linkedin.com/in/nandini-varshney-663995215/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Github Profile",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is my Github Profile where you can find my projects and contributions.",
+    url: "https://github.com/NandiniVar",
   },
   {
-    title: "My Resume Site",
+    title: "My Story telling Video",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This is my story telling video at Youtube ",
+    url: "https://www.youtube.com/watch?v=18K9MZIrtYc",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "My GFG video",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is my Geeks for Geeks video at Youtube to spread knowledge ",
+    url: "https://www.youtube.com/watch?v=PNc4SFO6gp8",
   },
 ];
 
