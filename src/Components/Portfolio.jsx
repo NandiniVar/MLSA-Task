80d6c28da26101e7483538c268c6fd54f6595622
Nandini Varshney <92244676+NandiniVar@users.noreply.github.com>
2023-11-06 18:58:12 +0530

    Update image imports and alt text

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "";
 
-const imageAltText = "study books image";
 
 /**
  * Project list
